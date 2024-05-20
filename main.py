196cc4247ee404f5af66b6844f76351f1801c86c
Yauheni Lampe <spakv49@gmail.com>
2024-05-20 20:42:10 +0500

    Completed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from src.widget import mask_bank_data
+from src.widget import get_data, mask_bank_data
 
 print(mask_bank_data("Visa Platinum 8990922113665229"))
+print(get_data("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -4,7 +4,13 @@ from .masks import get_masked_nums
 def mask_bank_data(bank_data: str) -> str:
     """Функция принимает данные и возвращает данные замаскированным номером карты/счета"""
     data_parts = bank_data.split()
-
     data_parts[-1] = get_masked_nums(data_parts[-1])
-
     return " ".join(data_parts)
+
+
+def get_data(date_string: str) -> str:
+    """Функция преобразует дату формата ГГГГ-ММ-ДД в формат ДД-ММ-ГГГГ."""
+    date = date_string.split("T", 1)[0]
+    formated_date_list = date.split("-")
+    year, month, day = formated_date_list
+    return f"{day}.{month}.{year}"
