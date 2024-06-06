bfa6074110d7d237f3241c2e6beb64a3a798fdb6
Yauheni Lampe <spakv49@gmail.com>
2024-06-06 20:43:35 +0500

    добавил  тесты в модуль test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -9,7 +9,7 @@ def log(filename=None):
                 result = func(*args, **kwargs)
                 log_message = f"{func.__name__} ok\n"
                 if filename:
-                    with open(filename, 'a') as f:
+                    with open(filename, "a") as f:
                         print(log_message, file=f)
                 else:
                     print(log_message)
@@ -17,7 +17,7 @@ def log(filename=None):
             except Exception as e:
                 error_message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}\n"
                 if filename:
-                    with open(filename, 'a') as f:
+                    with open(filename, "a") as f:
                         print(error_message, file=f)
                 else:
                     print(error_message)
