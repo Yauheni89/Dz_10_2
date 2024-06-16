3062cc864b8fbd4b35bc70194b03eba76c1b7050
Yauheni Lampe <spakv49@gmail.com>
2024-06-16 15:09:53 +0500

    Создал создал функциюread_transactions в модуле utils.py

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+
+def read_transactions(file_path):
+    """
+    Функция, которая читает данные о финансовых транзакциях из JSON-файла.
+
+    Args:
+        file_path (str): Путь до JSON-файла.
+
+    Returns:
+        list: Список словарей с данными о финансовых транзакциях.
+    """
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            if isinstance(data, list):
+                return data
+            else:
+                return []
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
