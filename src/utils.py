705aaebb030c09b4ec435a68bdcc56571df47675
Yauheni Lampe <spakv49@gmail.com>
2024-06-17 21:55:51 +0500

    переименовал функцию на read_operations в модуле utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,28 @@
 import json
 
 
-def read_transactions(file_path):
+def read_operations(file_path):
     """
-    Функция, которая читает данные о финансовых транзакциях из JSON-файла.
+    Читает данные о финансовых операциях из JSON-файла.
 
-    Args:
-        file_path (str): Путь до JSON-файла.
+    Аргументы:
+    file_path (str): Путь до JSON-файла с данными о финансовых операциях.
 
-    Returns:
-        list: Список словарей с данными о финансовых транзакциях.
+    Возвращает:
+    list: Список словарей с данными о финансовых операциях.
+    Если файл пустой, содержит не список или не найден,
+    возвращается пустой список.
     """
     try:
-        with open(file_path, 'r') as file:
+        # Открываем файл в режиме чтения с кодировкой 'utf-8'
+        with open(file_path, "r", encoding="utf-8") as file:
+            # Загружаем содержимое файла как JSON
             data = json.load(file)
+            # Проверяем, является ли загруженные данные списком
             if isinstance(data, list):
                 return data
             else:
                 return []
     except (FileNotFoundError, json.JSONDecodeError):
+        # Возвращаем пустой список в случае ошибки (файл не найден или ошибка при чтении JSON)
         return []
