823feec8d7c4a1e42555b12ff9cb6aa757ba8956
Yauheni Lampe <spakv49@gmail.com>
2024-06-21 21:15:02 +0500

    Создал логер для функций в модуле masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,23 +1,41 @@
+import logging
+
+# Настройка логирования
+logging.basicConfig(
+    filename='masks.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filemode='w'
+)
+
+logger = logging.getLogger('masks')
+
+
 def mask_card_numbers(nums: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    return f"{nums[:4]} {nums[4:6]}** **** {nums[-4:]}"
+    logger.debug(f"Получение замаскированного номера для: {nums}")
+    masked = f"{nums[:4]} {nums[4:6]}** **** {nums[-4:]}"
+    logger.debug(f"Получение замаскированного номера для: {masked}")
+    return masked
 
 
 def mask_account_numbers(nums: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    return f"**{nums[-4:]}"
+    logger.debug(f"Маскировка номера счета: {nums}")
+    masked = f"**{nums[-4:]}"
+    logger.debug(f"Маскировка номера счета: {masked}")
+    return masked
 
 
 def get_masked_nums(nums: int | str) -> str:
     """Функция определяет введен номер карты или номер счета."""
-
+    logger.debug(f"Получение замаскированного номера для: {nums}")
     nums = str(nums)
 
     if len(nums) == 16 and nums.isdigit():
         return mask_card_numbers(nums)
-
     elif len(nums) == 20 and nums.isdigit():
         return mask_account_numbers(nums)
-
     else:
+        logger.warning(f"Неверный входной номер: {nums}")
         return "Введите 16 или 20-значное число"
